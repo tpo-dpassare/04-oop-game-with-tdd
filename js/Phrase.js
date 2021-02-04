c58163f3221a138626e7b2aefd2ccbc2f8f1d66b
David Passarelli <dpassare@travelers.com>
2021-02-03 22:46:13 -0500

    Building tests for addPhraseToDisplay.

--- js/Phrase.js
+++ js/Phrase.js
@@ -11,4 +11,13 @@ class Phrase {
   get phrase () {
     return this._phrase
   }
+
+  addPhraseToDisplay () {
+    for (let i = 0; i < this._phrase.length; i++) {
+      window.ui.phraseList.insertAdjacentHTML(
+        'beforeend',
+        window.templates.render(window.templates.letter, this._phrase.charAt(i))
+      )
+    }
+  }
 }
--- js/Phrase.spec.js
+++ js/Phrase.spec.js
@@ -9,7 +9,7 @@ describe('meets expectations', () => {
   describe('the Phrase class', () => {
     it('must exist', () => {
       expect(() => {
-        const instance = new Phrase()
+        const instance = new Phrase() /* eslint-disable-line no-unused-vars */
       }).to.not.throw(/Phrase is not defined/)
     })
 
@@ -24,6 +24,81 @@ describe('meets expectations', () => {
         const actual = instance.phrase
 
         expect(actual).to.equal('test')
+      })
+    })
+
+    it('must have a property called "addPhraseToDisplay"', () => {
+      const instance = new Phrase()
+      expect(instance).to.have.property('addPhraseToDisplay')
+    })
+
+    describe('the "addPhraseToDisplay" property', () => {
+      let instance = null
+
+      before(() => {
+        instance = new Phrase('test')
+      })
+
+      it('must be a function', () => {
+        const expected = 'function'
+        const actual = typeof instance.addPhraseToDisplay
+
+        expect(actual).to.equal(expected)
+      })
+
+      context('when called', () => {
+        let parentElement = null
+
+        before(() => {
+          instance.addPhraseToDisplay()
+          parentElement = document.querySelector('#phrase UL')
+        })
+
+        after(() => {
+          // parentElement.innerHTML = ''
+        })
+
+        it('must create an element for each letter', () => {
+          const expected = instance.phrase.length
+          const actual = parentElement.children.length
+
+          expect(actual).to.equal(expected)
+        })
+
+        describe('each element', () => {
+          let elements = null
+
+          before(() => {
+            elements = parentElement.children
+          })
+
+          it('must be an LI', () => {
+            elements.forEach((elem) => {
+              const expected = 'LI'
+              const actual = elem.tagName
+
+              expect(actual).to.equal(expected)
+            })
+          })
+
+          it('must contain the appropriate letter', () => {
+            elements.forEach((elem, idx) => {
+              const expected = instance.phrase.charAt(idx)
+              const actual = elem.textContent
+
+              expect(actual).to.equal(expected)
+            })
+          })
+
+          it('must have the expected CSS class names', () => {
+            elements.forEach((elem, idx) => {
+              const expected = ['hide', 'letter']
+              const actual = elem.className.split(' ')
+
+              expect(actual).to.have.members(expected)
+            })
+          })
+        })
       })
     })
   })
